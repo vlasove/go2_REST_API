022d3f7a184c242da65d8d691ed971da31c6bfc3
EvVl <evgeny_vlasov@yahoo.com>
2020-08-18 19:33:26 +0300

    initial commit

---
+++ lec1/commander.sh
@@ -0,0 +1,8 @@
+
+# Обновить конфигурацию ОС
+sudo apt-get update
+sudo apt-get -y upgrade
+
+# Скачать дистрибутив компилятора
+wget https://golang.org/dl/go1.15.linux-amd64.tar.gz
+.....
---
+++ lec1/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+//HomePage function .....
+func HomePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Welcome to Home page of ULTRA RESTAPI!!!!</h1>")
+	fmt.Println("Hint: HomePage now linking....")
+}
+
+//GracefulShutdown function ....
+func GracefulShutdown() {
+	fmt.Println("REST API V1.0 shutting down....")
+}
+
+func main() {
+	defer GracefulShutdown()
+	fmt.Println("REST API V1.0 worked....")
+
+	http.HandleFunc("/", HomePage)
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}

0d945dd1ab89bc6614819d5ec44a25f2e89f8df1
Evgeny Vlasov <33553003+vlasove@users.noreply.github.com>
2020-08-20 16:29:39 +0300

    Added pre-test

---
+++ lec6/main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Item struct {
+	ID      string `json:"id"`
+	Content string `json:"content"`
+}
+
+//Items ...
+var Items []Item = []Item{
+	Item{"1", "abcd"},
+}
+
+//GetItems ...
+func GetItems(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(Items)
+}
+
+//GetItemID ...
+func GetItemID(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+	find := false
+	for _, item := range Items {
+		if item.ID == id {
+			find = true
+			json.NewEncoder(w).Encode(item)
+		}
+	}
+	if !find {
+		w.WriteHeader(http.StatusNotFound) // Изменить статус код запроса на 404
+	}
+}
+
+//PostItem ...
+func PostItem(w http.ResponseWriter, r *http.Request) {
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var item Item
+	json.Unmarshal(reqBody, &item)
+	w.WriteHeader(http.StatusCreated)
+	Items = append(Items, item)
+}
+
+//PutItemID ...
+func PutItemID(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+
+	finded := false
+
+	for index, item := range Items {
+		if item.ID == id {
+			finded = true
+			reqBody, _ := ioutil.ReadAll(r.Body)
+			w.WriteHeader(http.StatusAccepted)     // Изменяем статус код на 202
+			json.Unmarshal(reqBody, &Items[index]) // перезаписываем всю информацию для статьи с Id
+		}
+	}
+
+	if !finded {
+		w.WriteHeader(http.StatusNotFound) // Изменяем статус код на 404
+	}
+
+}
+
+//DeleteItemID ...
+func DeleteItemID(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+
+	find := false
+
+	for index, item := range Items {
+		if item.ID == id {
+			find = true
+			w.WriteHeader(http.StatusAccepted) // Изменить статус код на 202
+			Items = append(Items[:index], Items[index+1:]...)
+		}
+	}
+	if !find {
+		w.WriteHeader(http.StatusNotFound) // Изменить статус код на 404
+	}
+
+}
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/items", GetItems).Methods("GET")
+	router.HandleFunc("/item/{id}", GetItemID).Methods("GET")
+
+	router.HandleFunc("/item", PostItem).Methods("POST")
+
+	router.HandleFunc("/item/{id}", PutItemID).Methods("PUT")
+
+	router.HandleFunc("/item/{id}", DeleteItemID).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
---
+++ lec6/main_test.go
@@ -0,0 +1,167 @@
+package main
+
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGetItems(t *testing.T) {
+	request, err := http.NewRequest("GET", "/items", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	recorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(GetItems)
+	handler.ServeHTTP(recorder, request)
+
+	if status := recorder.Code; status != http.StatusOK {
+		t.Errorf("Handler returned %v, expected %v", status, http.StatusOK)
+	}
+
+	//Возвращает не нулевуой ответ
+	if len(recorder.Body.String()) <= 1 {
+		t.Errorf("Handler returned %v", recorder.Body.String())
+	}
+}
+
+func TestGetItemID(t *testing.T) {
+	request, err := http.NewRequest("GET", "/item", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	q := request.URL.Query()
+	q.Add("id", "1")
+	request.URL.RawQuery = q.Encode()
+
+	recorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(GetItemID)
+	handler.ServeHTTP(recorder, request)
+
+	if status := recorder.Code; status != http.StatusOK {
+		t.Errorf("Handler returned %v, expected %v", status, http.StatusOK)
+	}
+
+	if len(recorder.Body.String()) <= 1 {
+		t.Errorf("Handler returned %v", recorder.Body.String())
+	}
+}
+
+func TestGetItemIDDoesntExists(t *testing.T) {
+	request, err := http.NewRequest("GET", "/item", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	q := request.URL.Query()
+	q.Add("id", "123")
+	request.URL.RawQuery = q.Encode()
+
+	recorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(GetItemID)
+	handler.ServeHTTP(recorder, request)
+
+	if status := recorder.Code; status != http.StatusNotFound {
+		t.Errorf("Handler returned %v, expected %v", status, http.StatusNotFound)
+	}
+
+}
+
+func TestPostItem(t *testing.T) {
+	var jsonFIle = []byte(`{"id":"2","content":"dfge"}`)
+	request, err := http.NewRequest("POST", "/item", bytes.NewBuffer(jsonFIle))
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+	recorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(PostItem)
+	handler.ServeHTTP(recorder, request)
+
+	if status := recorder.Code; status != http.StatusCreated {
+		t.Errorf("Handler returned wrong %v code", status)
+	}
+}
+
+func TestDeleteItemIDDoesntExist(t *testing.T) {
+	request, err := http.NewRequest("DELETE", "/item", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	q := request.URL.Query()
+	q.Add("id", "200")
+	request.URL.RawQuery = q.Encode()
+	recorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(DeleteItemID)
+	handler.ServeHTTP(recorder, request)
+	if status := recorder.Code; status != http.StatusNotFound {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusNotFound)
+	}
+}
+
+func TestPutItemID(t *testing.T) {
+	var jsonFIle = []byte(`{"id":"1","content":"dfge"}`)
+	request, err := http.NewRequest("PUT", "/item", bytes.NewBuffer(jsonFIle))
+	q := request.URL.Query()
+	q.Add("id", "1")
+	request.URL.RawQuery = q.Encode()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+	recorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(PutItemID)
+	handler.ServeHTTP(recorder, request)
+
+	if status := recorder.Code; status != http.StatusAccepted {
+		t.Errorf("Handler returned wrong %v code", status)
+	}
+
+}
+
+func TestPutItemIDdOESNTeXIST(t *testing.T) {
+	var jsonFIle = []byte(`{"id":"200","content":"dfge"}`)
+	request, err := http.NewRequest("PUT", "/item", bytes.NewBuffer(jsonFIle))
+	q := request.URL.Query()
+	q.Add("id", "200")
+	request.URL.RawQuery = q.Encode()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+	recorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(PutItemID)
+	handler.ServeHTTP(recorder, request)
+
+	if status := recorder.Code; status != http.StatusNotFound {
+		t.Errorf("Handler returned wrong %v code", status)
+	}
+
+}
+
+func TestDeleteItemID(t *testing.T) {
+	request, err := http.NewRequest("DELETE", "/item", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	q := request.URL.Query()
+	q.Add("id", "1")
+	request.URL.RawQuery = q.Encode()
+	recorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(DeleteItemID)
+	handler.ServeHTTP(recorder, request)
+	if status := recorder.Code; status != http.StatusAccepted {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusAccepted)
+	}
+}
